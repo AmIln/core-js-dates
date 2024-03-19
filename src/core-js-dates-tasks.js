943583d63836a8e2ba776814be51814480234df7
Dmitry Andriyanov <facklak@mail.ru>
2024-03-19 13:10:56 +0300

    feat: did function getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,8 +32,23 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const newDate = new Date(date);
+  let hours = newDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = newDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let seconds = newDate.getSeconds();
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  const result = `${hours}:${minutes}:${seconds}`;
+
+  return result;
 }
 
 /**
